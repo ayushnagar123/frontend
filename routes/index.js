5ebe09eb7712b1d3375ce34fc9fa3fe0822a37d3
aksharanigam1112 <aksharanigam111298@gmail.com>
2019-11-24 11:29:43 +0530

    login signup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login',(req,res)=>
+{
+  res.render('login')
+})
+
+router.get('/signup',(req,res)=>
+{
+  res.render('signup')
+})
+
+router.post('/login',(req,res)=>
+{
+  name = req.body.name;
+  pass = req.body.pass;
+  console.log(name,pass)
+})
+
+router.post('/signup',(req,res)=>
+{
+  name = req.body.name;
+  pass = req.body.pass;
+  confirm = req.body.confirm_pass;
+  if(pass == confirm)
+    res.write('Successful')
+  else
+    res.write('Unsuccessful')
+  res.end()
+})
 module.exports = router;
